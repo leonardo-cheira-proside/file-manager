674dc8942b43f77078a5b09ef2f94e6a2a974f55
Leonardo <leonardo.cheira.ext@proside.pt>
2026-06-17 13:43:01 +0100

    remote tailwind cdn

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@lang('file-manager::file-manager.title')</title>
-    <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="{{ asset('vendor/file-manager/file-manager.css') }}">
     @livewireStyles
 </head>
