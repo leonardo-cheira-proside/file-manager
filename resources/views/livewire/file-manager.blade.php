14af70db847edc6e1e85fb23024462a650444085
Leonardo <leonardo.cheira.ext@proside.pt>
2026-06-17 12:30:04 +0100

    update img

--- resources/views/livewire/file-manager.blade.php
+++ resources/views/livewire/file-manager.blade.php
@@ -10,7 +10,7 @@
                     wire:click="$toggle('showTree')" title="@lang('file-manager::file-manager.directories')">
                 <svg class="w-6 h-6" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16"/></svg>
             </button>
-            <span class="font-semibold tracking-wide">{{ asset('images/logo.png') }}</span>
+            <img src="{{ asset('images/logo.png') }}" class=""/>
         </div>
 
         {{-- Pesquisa (filtra a vista atual no cliente) --}}
