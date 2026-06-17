64a5681b52606fb58e9372843bf8f7e3cacb6c64
Leonardo <leonardo.cheira.ext@proside.pt>
2026-06-17 14:50:29 +0100

    another one

--- resources/views/livewire/partials/context-menu.blade.php
+++ resources/views/livewire/partials/context-menu.blade.php
@@ -14,7 +14,7 @@
             <span>@lang('file-manager::file-manager.restore')</span>
         </button>
         <div class="border-t border-gray-100 my-1"></div>
-        <button type="button" @click="$wire.deleteForever(targetPaths()); menu.open=false"
+        <button type="button" @click="$wire.deleteForever(targetPaths()); selected=[]; menu.open=false"
                 class="w-full text-left px-4 py-2.5 text-red-600 hover:bg-teal-50 flex items-center gap-2">
             <svg class="w-4 h-4" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/></svg>
             <span>@lang('file-manager::file-manager.delete_forever')</span>
