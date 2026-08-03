df32a9734bc9c7441a77bed194b704386af18127
Leonardo <leonardo.cheira.ext@proside.pt>
2026-08-03 10:08:34 +0100

    fix: update icon component reference in picker view

--- resources/views/components/picker.blade.php
+++ resources/views/components/picker.blade.php
@@ -130,7 +130,7 @@
                 <button type="button" @click="open = false"
                     class="flex items-center justify-center w-10 h-10 rounded-md text-gray-500 hover:bg-gray-100 hover:text-gray-700 transition-colors duration-200"
                     aria-label="Fechar">
-                    <x-icons.cross class="w-5 h-5" />
+                    <x-file-manager::icons.cross class="w-5 h-5" />
                 </button>
             </div>
             <div class="flex-1 overflow-hidden">
